811d3401fd5ea948c841f463a9121383af3debb5
steven0129 <h3584935@gmail.com>
2024-01-16 14:22:19 +0800

    Support multiple types of LLM
    Signed-off-by: steven0129 <h3584935@gmail.com>

---
+++ run_llm.py
@@ -0,0 +1,32 @@
+from transformers import AutoTokenizer
+import transformers
+import torch
+
+model = 'Llama-2-7b-chat-hf'
+
+tokenizer = AutoTokenizer.from_pretrained('model/' + model)
+pipeline = transformers.pipeline(
+    "text-generation",
+    model=model,
+    torch_dtype=torch.float16,
+    device_map="auto",
+)
+
+
+if __name__ == '__main__':
+    buffer = ''
+
+    while True:
+        prompt = input('> ')
+        if prompt == 'exit': break
+        sequences = pipeline(
+            f"{buffer}[INST]{prompt}[/INST]",
+            do_sample=True,
+            top_k=10,
+            num_return_sequences=1,
+            eos_token_id=tokenizer.eos_token_id,
+            max_length=5000,
+        )
+
+        print(sequences[0]['generated_text'].split('[/INST]')[-1])
+        buffer = sequences[0]['generated_text']
